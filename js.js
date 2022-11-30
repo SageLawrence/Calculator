1714b0c38f9896529dad8611f868d98190fa3e57
Sage Lawrence <sageblawrence@gmail.com>
2022-11-29 19:08:28 -0800

    Created initial files and added basic math functions to JS

---
+++ js.js
@@ -0,0 +1,18 @@
+
+// this section contains simple math functions
+function add(num, num2) {
+    return num + num2;
+}
+
+function subtract(num, num2) {
+    return num - num2;
+}
+
+function multiply(num, num2) {
+    return num * num2;
+}
+
+function divide(num, num2) {
+    return num / num2;
+}
+// this section contains simple math functions
